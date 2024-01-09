8a9ef3ae6bc793010c07737f64fb2686c036f0eb
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2024-01-09 16:18:59 +0100

    Rework CompletionHandler to avoid subclassing a functional type
    Workaround for KT-64075

--- kotlinx-coroutines-core/common/src/Job.kt
+++ kotlinx-coroutines-core/common/src/Job.kt
@@ -357,7 +357,7 @@ public interface Job : CoroutineContext.Element {
     public fun invokeOnCompletion(
         onCancelling: Boolean = false,
         invokeImmediately: Boolean = true,
-        handler: CompletionHandler): DisposableHandle
+        handler: InternalCompletionHandler): DisposableHandle
 
     // ------------ unstable internal API ------------
 
@@ -491,7 +491,7 @@ public interface ChildHandle : DisposableHandle {
  * ```
  */
 internal fun Job.disposeOnCompletion(handle: DisposableHandle): DisposableHandle =
-    invokeOnCompletion(handler = DisposeOnCompletion(handle).asHandler)
+    invokeOnCompletion(handler = DisposeOnCompletion(handle))
 
 /**
  * Cancels the job and suspends the invoking coroutine until the cancelled job is complete.
